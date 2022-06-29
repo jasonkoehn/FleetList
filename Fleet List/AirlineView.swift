7a236b3170544dac5a763e7871d48579019a6e3d
jasonkoehn <jasonkoehn04@gmail.com>
2022-06-29 08:09:28 -0700

    three

---
+++ Fleet List/AirlineView.swift
@@ -0,0 +1,20 @@
+//
+//  AirlineView.swift
+//  Fleet List
+//
+//  Created by Jason Koehn on 6/29/22.
+//
+
+import SwiftUI
+
+struct AirlineView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AirlineView_Previews: PreviewProvider {
+    static var previews: some View {
+        AirlineView()
+    }
+}
---
+++ Fleet List/FleetView.swift
@@ -0,0 +1,20 @@
+//
+//  FleetView.swift
+//  Fleet List
+//
+//  Created by Jason Koehn on 6/29/22.
+//
+
+import SwiftUI
+
+struct FleetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FleetView_Previews: PreviewProvider {
+    static var previews: some View {
+        FleetView()
+    }
+}
