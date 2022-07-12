e62fe7f8a5dbaf785618b47d89174cf15b403f0a
jasonkoehn <jasonkoehn04@gmail.com>
2022-07-11 17:41:08 -0700

    updates

--- Fleet List/AircraftView.swift
+++ Fleet List/AircraftView.swift
@@ -20,74 +20,83 @@ struct AircraftView: View {
     var fn: Int
     var body: some View {
         VStack {
+            Image(name)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding()
+                .frame(height: 100)
             Text(registration)
                 .font(.system(size: 50))
                 .padding(30)
             Text(model)
                 .font(.system(size: 35))
                 .padding(.bottom)
-            HStack {
-                Text("Operator:")
-                    .font(.system(size: 25))
-                    .italic()
-                Spacer()
-                Text(name)
-                    .font(.system(size: 25))
-                Spacer()
-            }
-            HStack {
-                Text("Type:")
-                    .font(.system(size: 25))
-                    .italic()
-                Spacer()
-                Text(type)
-                    .font(.system(size: 25))
-                Spacer()
-            }
-            HStack {
-                Text("Delivery:")
-                    .font(.system(size: 25))
-                    .italic()
-                Spacer()
-                Text(delivery_date)
-                    .font(.system(size: 25))
-                Spacer()
-            }
-            HStack {
-                Text("Hex:")
-                    .font(.system(size: 25))
-                    .italic()
-                Spacer()
-                Text(hex)
-                    .font(.system(size: 25))
-                Spacer()
-            }
-            HStack {
-                Text("MSN:")
-                    .font(.system(size: 25))
-                    .italic()
-                Spacer()
-                Text("\(msn)")
-                    .font(.system(size: 25))
-                Spacer()
-            }
-            HStack {
-                Text("LN:")
-                    .font(.system(size: 25))
-                    .italic()
-                Spacer()
-                Text("\(ln)")
-                    .font(.system(size: 25))
-                Spacer()
-            }
-            HStack {
-                Text("FN:")
-                    .font(.system(size: 25))
-                    .italic()
-                Spacer()
-                Text("\(fn)")
-                    .font(.system(size: 25))
-                Spacer()
+            VStack {
+                HStack {
+                    Text("Operator:")
+                        .font(.system(size: 25))
+                        .italic()
+                    Spacer()
+                    Text(name)
+                        .font(.system(size: 25))
+                    Spacer()
+                }
+                HStack {
+                    Text("Type:")
+                        .font(.system(size: 25))
+                        .italic()
+                    Spacer()
+                    Text(type)
+                        .font(.system(size: 25))
+                    Spacer()
+                }
+                HStack {
+                    Text("Delivery:")
+                        .font(.system(size: 25))
+                        .italic()
+                    Spacer()
+                    Text(delivery_date)
+                        .font(.system(size: 25))
+                    Spacer()
+                }
+                HStack {
+                    Text("Hex:")
+                        .font(.system(size: 25))
+                        .italic()
+                    Spacer()
+                    Text(hex)
+                        .font(.system(size: 25))
+                    Spacer()
+                }
+                HStack {
+                    Text("MSN:")
+                        .font(.system(size: 25))
+                        .italic()
+                    Spacer()
+                    Text("\(msn)")
+                        .font(.system(size: 25))
+                    Spacer()
+                }
+                if ln != 0 {
+                    HStack {
+                        Text("LN:")
+                            .font(.system(size: 25))
+                            .italic()
+                        Spacer()
+                        Text("\(ln)")
+                            .font(.system(size: 25))
+                        Spacer()
+                    }
+                }
+                HStack {
+                    Text("FN:")
+                        .font(.system(size: 25))
+                        .italic()
+                    Spacer()
+                    Text("\(fn)")
+                        .font(.system(size: 25))
+                    Spacer()
+                }
             }
             Spacer()
         }
@@ -97,7 +106,7 @@ struct AircraftView: View {
 struct AircraftView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
-            AircraftView(name: "Southwest Airlines", picture_url: "", type: "B38M", model: "Boeing 737 Max 8", registration: "N8701L", delivery_date: "2018-02-20", hex: "A1A87E", msn: 55389, ln: 4480, fn: 8701)
+            AircraftView(name: "JetBlue Airways", picture_url: "", type: "B38M", model: "Boeing 737 Max 8", registration: "N8701L", delivery_date: "2018-02-20", hex: "A1A87E", msn: 55389, ln: 4480, fn: 8701)
         }
     }
 }
