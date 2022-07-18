2484596898a40adc9c14b8b166d5307f3b56f5d9
jasonkoehn <jasonkoehn04@gmail.com>
2022-07-17 17:42:47 -0700

    added url

--- Fleet List/AirlineView.swift
+++ Fleet List/AirlineView.swift
@@ -21,29 +21,64 @@ struct AirlineView: View {
             Image(alias)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
-                .frame(height: 80)
-                .padding(.horizontal, 10)
-            HStack {
-                Spacer()
-                Text("IATA")
-                    .italic()
-                    .font(.subheadline)
-                Text(iata)
-                Spacer()
-                Text("ICOA")
-                    .italic()
-                    .font(.subheadline)
-                Text(icao)
-                Spacer()
-                Text("Callsign")
-                    .italic()
-                    .font(.subheadline)
-                Text(callsign)
-                Spacer()
-            }
-            Text(website)
-            Text("\(fleetsize)")
+                .frame(height: 150)
+                .padding(10)
+            Text(name)
+                .font(.title)
+                .padding(.bottom)
+                .padding(.horizontal)
+                HStack {
+                    Spacer()
+                    Text("IATA")
+                        .italic()
+                        .font(.headline)
+                    Text(iata)
+                        .font(.system(size: 20))
+                    Spacer()
+                    Text("ICOA")
+                        .italic()
+                        .font(.headline)
+                    Text(icao)
+                        .font(.system(size: 20))
+                    Spacer()
+                }
+                HStack {
+                    Spacer()
+                    Text("CALLSIGN")
+                        .italic()
+                        .font(.headline)
+                    Text(callsign)
+                        .font(.system(size: 20))
+                    Spacer()
+                }.padding(3)
+                HStack {
+                    Spacer()
+                    Text("Website")
+                        .italic()
+                        .font(.headline)
+                    Link(website, destination: URL(string: "https://www." + website)!)
+                        .font(.system(size: 22))
+                    Spacer()
+                }
+                HStack {
+                    Spacer()
+                    Text("Fleet Size")
+                        .italic()
+                        .font(.headline)
+                    Text("\(fleetsize)")
+                        .font(.system(size: 20))
+                    Spacer()
+                }.padding(3)
+            Spacer()
         }
         
     }
 }
+
+struct AirlineView_Previews: PreviewProvider {
+    static var previews: some View {
+        AirlineView(name: "Southwest Airlines", country: "United States", website: "southwest.com", iata: "WN", icao: "SWA", callsign: "SOUTHWEST", alias: "SouthwestAirlines", fleetsize: 735)
+    }
+}
+// AveloAir
+//SouthwestAirlines
